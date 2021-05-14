57ca1c273883870f596c6358e4580f1261bd62a9
Bono Nonchev <11063463+madcowbg@users.noreply.github.com>
2021-05-14 10:48:17 -0400

    a better random chart

--- src/preload.ts
+++ src/preload.ts
@@ -1,5 +1,54 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+
+import * as _ from "lodash"
+import * as fs from "fs"
+import {Chart} from "chart.js"
+
+import * as d3 from "d3-random"
+const normalRNG = d3.randomNormal.source(d3.randomLcg(1251241))(0, 1);
+
+function createCharts() {
+  const root = fs.readdirSync('/')
+
+  console.log(root)
+
+  console.log("renderer executing!");
+
+  (document.getElementById('mytext') as HTMLDivElement).textContent = "Edited!"
+
+  const ctx = (document.getElementById('myChart') as HTMLCanvasElement).getContext('2d');
+
+  const returnsData = _.map(_.range(0, 100), normalRNG);
+
+  const pricesData = [0];
+  for (let i = 0; i < returnsData.length; i++) {
+    pricesData.push(pricesData[pricesData.length-1] + returnsData[i]);
+  }
+
+  const days = _.map(_.range(0, 100), (v) => "day " + (v + 1));
+
+  const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: days,
+      datasets: [{
+        label: 'Price',
+        data: pricesData,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      maintainAspectRatio: true,
+      responsive: false
+    }
+  });
+
+  chart.update({duration: 500, lazy: false, easing: 'linear'});
+
+  console.log(chart.getDatasetMeta(0));
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   const replaceText = (selector: string, text: string) => {
     const element = document.getElementById(selector);
@@ -12,50 +61,5 @@ window.addEventListener("DOMContentLoaded", () => {
     replaceText(`${type}-version`, process.versions[type as keyof NodeJS.ProcessVersions]);
   }
 
-  const fs = require('fs')
-
-  const root = fs.readdirSync('/')
-
-  console.log(root)
-
-  const {Chart} = require("chart.js");
-
-  console.log("renderer executing!");
-
-  (document.getElementById('mytext') as HTMLDivElement).textContent = "Edited!"
-
-  const ctx = (document.getElementById('myChart') as HTMLCanvasElement).getContext('2d');
-  const chart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {}
-  });
-
-  chart.update({duration: 500, lazy: false, easing: 'linear'});
-
-  console.log(chart.getDatasetMeta(0));
-
+  createCharts();
 });
